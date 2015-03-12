8caa19ca21b044bdab54f9ec253d5841d2b7cbab
Andrew Yang <evil_eve_live@outlook.com>
2015-03-12 10:23:09 +0800

    Update BlockTypes.h
    Reformat code.

--- BlockTypes.h
+++ BlockTypes.h
@@ -7,12 +7,12 @@
 //
 #ifndef __BlockTypes
 #define __BlockTypes
-typedef void (^blockVoidVoid)(void);
-typedef void (^blockVoidId)(id value1);
-typedef void (^blockVoidIdId)(id value1, id value2);
+typedef void (^blockVoidVoid)  (void);
+typedef void (^blockVoidId)    (id value1);
+typedef void (^blockVoidIdId)  (id value1, id value2);
 typedef void (^blockVoidIdIdId)(id value1, id value2, id value3);
-typedef id   (^blockIdVoid)(void);
-typedef id   (^blockIdId)(id value1);
-typedef id   (^blockIdIdId)(id value1, id value2);
-typedef id   (^blockIdIdIdId)(id value1, id value2, id value3);
+typedef id   (^blockIdVoid)    (void);
+typedef id   (^blockIdId)      (id value1);
+typedef id   (^blockIdIdId)    (id value1, id value2);
+typedef id   (^blockIdIdIdId)  (id value1, id value2, id value3);
 #endif
