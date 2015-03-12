49ebee6f31d86890081c485f1e637e754a384784
Andrew Yang <evil_eve_live@outlook.com>
2015-03-12 10:03:48 +0800

    Create BlockTypes.h
    Add basic block types

---
+++ BlockTypes.h
@@ -0,0 +1,8 @@
+typedef void (^blockVoidVoid)(void);
+typedef void (^blockVoidId)(id value1);
+typedef void (^blockVoidIdId)(id value1, id value2);
+typedef void (^blockVoidIdIdId)(id value1, id value2, id value3);
+typedef id   (^blockIdVoid)(void);
+typedef id   (^blockIdId)(id value1);
+typedef id   (^blockIdIdId)(id value1, id value2);
+typedef id   (^blockIdId)(id value1, id value2, id value3);
